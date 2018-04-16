bcd65813b4ce005ed905843b7fe22e2ed8991d3c
Siegfried Jahnel <JahnelS@ad.trw.com>
2018-04-16 14:18:58 +0200

    4th

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const competitions = [
 	{"name":"BL2", "id":"01VV7REICS00000DVS54898DVTPALDN0-G"},
   {"name":"BL3", "id":"01VM7AUE1K000000VS54898EVSV90M3P-G"},
 	{"name":"RLB", "id":"0200TN5NS0000007VS54898DVUL01S6C-G"},
-	{"name":"LLSO", "id":"020223LG98000000VS54898DVUL01S6C-G"}
+	{"name":"LLSO", "id":"020223LG98000000VS54898DVUL01S6C-G"},
+  {"name":"KLIS", "id":"0202NGC3OG000003VS54898EVSIMIKOR-G"}
 ];
 
 var matchday = 0;
 var selMatchday;
 var competitionId = competitions[0].id;
 
-const url = "http://bfv.de/rest/competitioncontroller";
+const url = "https://bfv.de/rest/competitioncontroller";
 
 const matchdayContainer = document.getElementById('matchdayContainer');
 const resultsTableBody = document.getElementById( 'resultsTableBody');
+const tableTableBody = document.getElementById( 'tableTableBody');
 const minusButton = document.getElementById('minusButton');
 const plusButton = document. getElementById('plusButton');
 const navButtons = document.getElementsByClassName('navButtons')
@@ -41,6 +43,14 @@ plusButton.addEventListener('click', function(){
 
 });
 
+//----------------------------------------------------------
+if ('serviceWorker' in navigator) { 
+  window.addEventListener('load', () => 
+    navigator.serviceWorker.register('sw.js') 
+      .catch(err => 'SW registration failed')); 
+ } 
+
+//----------------------------------------------------------
 
 //-------------------------------------------------------------
 function getMatchData(competitionId , matchday){
@@ -58,9 +68,11 @@ function getMatchData(competitionId , matchday){
     if(matchday == 0) selMatchday = actualMatchDay;
     console.log(staffelname + "ST: " + selMatchday);
     matchdayContainer.innerHTML = staffelname + " , " + selMatchday + ". ST";
-    resultsTableBody.innerHTML = json.data.matches.map(createOutput).join('\n');
+    resultsTableBody.innerHTML = json.data.matches.map(drawResultTable).join('\n');
+    tableTableHead.innerHTML = `<th>&nbsp;</th><th>Verein</th><th>Sp</th><th>Pu</th><th>TV</th>` ;
+    tableTableBody.innerHTML = json.data.tabelle.map(drawTableTable).join('\n');
 
-    //console.log(json);
+    console.log(json);
   
 })
 .catch(function(err) {
@@ -69,13 +81,23 @@ function getMatchData(competitionId , matchday){
 };
 //----------------------------------------------------------------
 
-function createOutput(match){
+function drawResultTable(match){
   return `<tr>
     <td>${match.kickoffDate.substring(0,5)}<br>
       ${match.kickoffTime}</td>
       <td>${match.homeTeamName.substring(0,24)}<br>
       ${match.guestTeamName.substring(0,24)}</td>
       <td>${match.result}</td>
+    </tr>`;
+}
+
+function drawTableTable(match){
+  return `<tr>
+    <td>${match.rang}</td>
+    <td>${match.teamname.substring(0,24)}</td>
+    <td>${match.anzspiele}</td>
+    <td>${match.punkte}</td>
+    <td>${match.tordiff}</td>
     </tr>`;
 }
 
---
+++ sw-v2.js
@@ -0,0 +1,40 @@
+
+  self.addEventListener('install', async function () { 
+   const cache = await caches.open('static-v1'); 
+   cache.addAll(['styles.css', 'index.html']); 
+ }); 
+ 
+ 
+ self.addEventListener('activate', event => { 
+   event.waitUntil(self.clients.claim()); 
+ }); 
+ 
+ 
+ self.addEventListener('fetch', event => { 
+   const request = event.request; 
+   const url = new URL(request.url); 
+   if (url.origin === location.origin) { 
+     event.respondWith(cacheFirst(request)); 
+   } else { 
+     event.respondWith(networkFirst(request)); 
+   } 
+ }); 
+ 
+ 
+ async function cacheFirst(request) { 
+   const cachedResponse = await caches.match(request); 
+   return cachedResponse || fetch(request); 
+ } 
+ 
+ 
+ async function networkFirst(request) { 
+   const dynamicCache = await caches.open('dynamic-v1'); 
+   try { 
+     const networkResponse = await fetch(request); 
+     dynamicCache.put(request, networkResponse.clone()); 
+     return networkResponse; 
+  } catch (err) { 
+     const cachedResponse = await dynamicCache.match(request); 
+     return cachedResponse || await caches.match('./fallback.json'); 
+   } 
+ } 
